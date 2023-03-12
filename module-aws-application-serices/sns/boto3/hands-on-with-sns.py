23da429e71d9565caaf828d2bcc6718cbd784f29
sparta <badrik7@gmail.com>
2023-03-12 11:09:06 +0000

    Added a delay prior to sns publish message

--- module-aws-application-serices/sns/boto3/hands-on-with-sns.py
+++ module-aws-application-serices/sns/boto3/hands-on-with-sns.py
@@ -1,4 +1,6 @@
 import boto3
+import time
+
 
 """
     In this example, the script first creates an SNS client using the boto3.client() method. It then uses the create_topic() method to create a new SNS topic and retrieves the topic ARN from the response. Next, the script sends a message to the topic using the publish() method, passing in the topic ARN and the message to send. Finally, the script subscribes to the topic using the subscribe() method, specifying the protocol (in this case, email) and the endpoint (in this case, a sample email address).
@@ -10,11 +12,14 @@ sns = boto3.client('sns')
 # Create an SNS topic
 topic_arn = sns.create_topic(Name='my-topic')['TopicArn']
 
-# Send a message to the topic
-message = 'Hello, world!'
-sns.publish(TopicArn=topic_arn, Message=message)
-
 # Subscribe to the topic
 protocol = 'email'
 endpoint = 'user@example.com'
 subscription_arn = sns.subscribe(TopicArn=topic_arn, Protocol=protocol, Endpoint=endpoint)['SubscriptionArn']
+
+# Sleep for 120 seconds
+time.sleep(120)
+
+# Send a message to the topic
+message = 'Hello, world!'
+sns.publish(TopicArn=topic_arn, Message=message)
